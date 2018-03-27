19261d1ec3fd8660ea475be63132899bc0273e4e
Jay Bednar <ironviralmedia@gmail.com>
2018-03-27 21:50:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    req = Rack::Request.new(env)
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
