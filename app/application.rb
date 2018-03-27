08261866a6e57f2da9774459c6da3515a4f22c46
Jay Bednar <ironviralmedia@gmail.com>
2018-03-27 21:49:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,16 @@
 class Application
-
+ @@items = ["Apples","Carrots","Pears"]
   def call(env)
     resp = Rack::Response.new
 
-
-    if Time.now.hour < 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+    @@items.each do |item|
+       resp.write "#{item}\n"
+     end
+    # if Time.now.hour < 12
+    #   resp.write "Good Morning!"
+    # else
+    #   resp.write "Good Afternoon!"
+    # end
 
     resp.finish
   end
